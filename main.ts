83f622099eac2c8804ad711798e75f4eaff3ef85
Barry149149 <44249492+Barry149149@users.noreply.github.com>
2022-07-04 11:01:44 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,279 @@
-basic.forever(function () {
-	
-})
+namespace ExtensionBoard {
+
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
+    const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+
+    let initialized = false
+
+    const PortDigi = [
+        [DigitalPin.P8, DigitalPin.P0],
+        [DigitalPin.P12, DigitalPin.P1],
+        [DigitalPin.P13, DigitalPin.P2],
+        [DigitalPin.P15, DigitalPin.P14],
+        [DigitalPin.P6, DigitalPin.P3],
+        [DigitalPin.P7, DigitalPin.P4],
+        [DigitalPin.P9, DigitalPin.P10]
+    ]
+
+    export enum Ports {
+        PORT1 = 0,
+        PORT2 = 1,
+        PORT3 = 2,
+        PORT4 = 3,
+        PORT5 = 4,
+        PORT6 = 5,
+        PORT7 = 6
+    }
+
+    export enum Slots {
+        A = 1, // inverse slot by zp
+        B = 0
+    }
+
+    export enum Motors {
+        M1 = 0x1,
+        M2 = 0x2
+    }
+
+    export enum DHT11Type {
+        //% block=temperature(°C)
+        TemperatureC = 0,
+        //% block=temperature(°F)
+        TemperatureF = 1,
+        //% block=humidity
+        Humidity = 2
+    }
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf2 = pins.createBuffer(1)
+        buf2[0] = value
+        pins.i2cWriteBuffer(addr, buf2)
+    }
+
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+        let buf3 = pins.createBuffer(5);
+        buf3[0] = LED0_ON_L + 4 * channel;
+        buf3[1] = on & 0xff;
+        buf3[2] = (on >> 8) & 0xff;
+        buf3[3] = off & 0xff;
+        buf3[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf3);
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        for (let idx = 0; idx < 16; idx++) {
+            setPwm(idx, 0, 0);
+        }
+        initialized = true
+    }
+
+    export function MotorRun(index: Motors, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        speed = speed * 16; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= -4096) {
+            speed = -4095
+        }
+        if (index > 2 || index <= 0)
+            return
+        let pp = (index - 1) * 2
+        let pn = (index - 1) * 2 + 1
+        // serial.writeString("M " + index + " spd " + speed + " pp " + pp + " pn " + pn + "\n")
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
+    }
+
+    //% blockId=dht11 block="DHT11|port %port|type %readtype"
+    //% weight=60
+    //% group="Environment" blockGap=50
+    export function DHT11(port: Ports, readtype: DHT11Type): number {
+        let dht11pin = PortDigi[port][0]
+
+        pins.digitalWritePin(dht11pin, 0)
+        basic.pause(18)
+        let i = pins.digitalReadPin(dht11pin)
+        pins.setPull(dht11pin, PinPullMode.PullUp);
+        switch (readtype) {
+            case 0:
+                let dhtvalue1 = 0;
+                let dhtcounter1 = 0;
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+                for (let j = 0; j <= 32 - 1; j++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter1 = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter1 += 1;
+                    }
+                    if (j > 15) {
+                        if (dhtcounter1 > 2) {
+                            dhtvalue1 = dhtvalue1 + (1 << (31 - j));
+                        }
+                    }
+                }
+                return ((dhtvalue1 & 0x0000ff00) >> 8);
+                break;
+            case 1:
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+                let dhtvalue = 0;
+                let dhtcounter = 0;
+                for (let k = 0; k <= 32 - 1; k++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter += 1;
+                    }
+                    if (k > 15) {
+                        if (dhtcounter > 2) {
+                            dhtvalue = dhtvalue + (1 << (31 - k));
+                        }
+                    }
+                }
+                return Math.round((((dhtvalue & 0x0000ff00) >> 8) * 9 / 5) + 32);
+                break;
+            case 2:
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+
+                let value = 0;
+                let counter = 0;
+
+                for (let l = 0; l <= 8 - 1; l++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    counter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        counter += 1;
+                    }
+                    if (counter > 3) {
+                        value = value + (1 << (7 - l));
+                    }
+                }
+                return value;
+            default:
+                return 0;
+        }
+    }
+
+    //% blockId=haha block="HaHa|huhu %hehe|huhu %hihi"
+    //% group="gaga" weight=81
+    export function HaHa(hehe: number, hihi: string): void {
+
+    }
+
+    //% blockId=hehe block="HeHe|hoho %hehe|hoho %hihi"
+    //% group="gaga" weight=81
+    export function HeHe(hehe: number, hihi: string): void {
+
+    }
+
+    //% blockId=hihi block="HiHi|hoho %hehe|hoho %hihi"
+    //% group="gaga" weight=81
+    export function HiHi(hehe: number, hihi: string): void {
+
+    }
+
+    //% blockId=hoho block="HoHo|hoho %hehe|hoho %hihi"
+    //% group="gaga" weight=81
+    export function HoHo(hehe: number, hihi: string): void {
+
+    }
+
+    //% blockId=huhu block="HuHu|hoho %hehe|hoho %hihi"
+    //% group="gaga" weight=81
+    export function HuHu(hehe: number, hihi: string): void {
+
+    }
+
+
+    //% blockId=custom_motor_dual block="Motor|speed of Motor1 %speed1|speed of Motor2 %speed2"
+    //% weight=43
+    //% speed1.min=-255 speed1.max=255
+    //% speed2.min=-255 speed2.max=255
+    //% group="Actuator" name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRunDual(speed1: number, speed2: number): void {
+        MotorRun(1, speed1);
+        MotorRun(2, speed2);
+    }
+
+    //% blockId=custom_tracer block="Tracer|port %port|slot %slot"
+    //% group="Linefollower" weight=81
+    export function Tracer(port: Ports, slot: Slots): boolean {
+        let pin = PortDigi[port][slot]
+        pins.setPull(pin, PinPullMode.PullUp)
+        return pins.digitalReadPin(pin) == 1
+    }
+
+    //% blockId=custom_tracing_line_with_motors block="MoveAlongBlackLine|port %port|speed %speed"
+    //% speed.min=-255 speed.max=255
+    export function TracingLineWithMotors(port: Ports, speed: number): void {
+        if (Tracer(port, Slots.A) && Tracer(port, Slots.B)) {
+            MotorRunDual(speed, speed)
+        } else if (!(Tracer(port, Slots.A)) && Tracer(port, Slots.B)) {
+            MotorRunDual(speed, -speed)
+        } else if (Tracer(port, Slots.A) && !(Tracer(port, Slots.B))) {
+            MotorRunDual(-speed, speed)
+        } else if (!(Tracer(port, Slots.A)) && !(Tracer(port, Slots.B))) {
+            MotorRunDual(-speed, -speed)
+        }
+    }
+}
